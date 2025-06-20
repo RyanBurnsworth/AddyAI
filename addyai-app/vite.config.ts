98921ad0c8798aa3d00b7ec8267d96004477eaf5
Ryan Burnsworth <ryanburnsworth@gmail.com>
2025-06-19 21:59:11 -0400

    updated configs

--- addyai-app/vite.config.ts
+++ addyai-app/vite.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: ['www.addyai.com', 'addyai.com'],
+  },
 });
